a0adb4a347ba6f831fc37e85de3e788b5b7d0526
Koded-P <edosepray@gmail.com>
2021-09-09 17:40:50 +0100

    latest commit

--- frontend/js/article.js
+++ frontend/js/article.js
@@ -2,6 +2,10 @@ const urlParam = new URLSearchParams(window.location.search);
 let productId = urlParam.get("id");
 let product = null;
 
+
+
+  // FETCH API
+
 fetch("http://localhost:3000/api/cameras/" + productId)
     .then((response) => {
         return response.json();
@@ -9,9 +13,7 @@ fetch("http://localhost:3000/api/cameras/" + productId)
 
 .then((data) => {
     console.log(data);
-    product = data;
-    
-      let container = document.querySelector('#productList');
+
 
       //HTML container
   
@@ -22,19 +24,24 @@ fetch("http://localhost:3000/api/cameras/" + productId)
           <div class="card-body"></div>
               <h3 class="card-title" >${data.name}</h3>
               <p class="card-text">${data.price}€</p>
-              <select class="lenses">${data.lenses}</select>
+              <select>
+                <option disabled hidden selected>select</option>
+                <option>${data.lenses}</option>
+              </select>
+              <h2>result</h2>
           </div>
       </div>
           `;
-  
+
       document.getElementById("productList").insertAdjacentHTML('beforeend',innerHTML) ;
       console.log(container);
 })
 
-
 .catch(function () {
   window.alert('oops something went wrong! Try again.');
 });
+
+
 
 //Add to Basket
 function AddtoCart() {
@@ -45,18 +52,18 @@ function AddtoCart() {
 
   let indexInBasket = basket.findIndex((item) => item._id === productId);
 
-  //index ==-1 signifie non trouvé
+  //index ==-1
   if (indexInBasket < 0) {
-    // si non présent dans le panier
+    // if not present in basket
     product.quantity = 1;
 
     basket.push(product);
-  } // produit déja présent, modifier quantité
+  } // product already present modify quantity
   else {
 
     basket[indexInBasket].quantity += 1;
   }
 
   SaveBasket(basket);
-  window.alert("Votre article a été ajouté au panier.");
+  window.alert("The item will be added to your Cart.");
 }
